6b8686545a405697e02058db0cef1ee7d33f7f58
Doritanh <charrier.antho99@gmail.com>
2019-09-12 23:01:29 +0200

    Authentification class

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,42 @@
 window.addEventListener('load', () => {
     var body : HTMLElement = document.body;
     body.classList.add('dark');
-})
+
+    var auth = new Authentification(body);
+    auth.show();
+});
+
+class Authentification {
+    private element : HTMLElement;
+    private parent : HTMLElement;
+
+    constructor(parent : HTMLElement) {
+        this.parent = parent;
+
+        this.element = document.createElement('form');
+        this.element.addEventListener('submit', (e) => { this.validate(e) })
+        this.element.classList.add('authentification');
+        
+        var pseudo = document.createElement('input');
+        pseudo.type = 'text';
+
+        this.element.appendChild(pseudo);
+
+        var password = document.createElement('input');
+        password.type = 'password';
+
+        this.element.appendChild(password);
+    }
+
+    private validate(event : Event) {
+        event.preventDefault();
+    }
+
+    public show() {
+        this.parent.appendChild(this.element);
+    }
+
+    public remove() {
+        this.element = this.parent.removeChild(this.element);
+    }
+}
